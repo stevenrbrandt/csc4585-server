d7f1e7a1baf11af8be04a59c12cbb456e52f9cb6
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-08-21 14:12:07 -0500

    Add missing files and update flour jar.

---
+++ startup.sh
@@ -0,0 +1,14 @@
+cd /
+randpass MND -o /usr/enable_mkuser
+python /usr/local/bin/frame.py
+
+for f in passwd shadow
+do
+    if [ -r /home/${f} ]
+    then
+        cp /home/${f} /etc/${f}
+    fi
+done
+
+PORT=443
+jupyterhub --ip 0.0.0.0 --port $PORT -f jup-config.py
